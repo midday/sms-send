bee703e0297ae5d43adfe55df7aae9051b244aa0
liuzhengwu02 <liuzhengwu02@meituan.com>
2020-11-21 20:22:55 +0800

    feat: 初次提交

---
+++ pages/index/index.vue
@@ -0,0 +1,265 @@
+<template>
+	<view class="container">
+		<view class="phone-wrap">
+			<view class="phone" @click="copyPhoneNumber">{{this.phoneInfo.phoneNumber || '请加载手机号'}}</view>
+			<view class="belong">{{belong || '归属地'}}</view>
+		</view>
+		<view class="salutation-wrap">
+			<label class="salutation-label">称谓：</label>
+			<input class="salutation-input" v-model="salutation" type="text" />
+		</view>
+		<view class="btn-wrap">
+			<button class="btn" type="default" @click="clickNotSendBtn">不发送，下一个</button>
+			<button class="btn" type="default" @click="clickSendBtn">发送，下一个</button>
+		</view>
+		<view class="btn-wrap">
+			<button class="btn" type="primary" @click="templateContent = item.content" v-for="item in templateList" :key="item._id">{{item.name}}</button>
+		</view>
+		<textarea class="template-content" placeholder="模板内容" v-model="templateContent" />
+		</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				userInfo: {}, // 用户信息
+				phoneInfo: {}, // 手机信息
+				salutation: '',// 称呼
+				templateContent: '',// 模板内容
+				templateList: []// 模板列表
+			}
+		},
+		onNavigationBarButtonTap() {
+			uni.setStorageSync('userInfo', {
+				...this.userInfo,
+				token: ''
+			})
+			plus.runtime.quit();
+		},
+		created() {
+			this.userInfo = uni.getStorageSync('userInfo') || {}
+			this.getTemplateList()
+		},
+		computed: {
+			belong() {
+				return this.phoneInfo.area && this.phoneInfo.sp ? `${this.phoneInfo.area} ${this.phoneInfo.sp}` : ''
+			}
+		},
+		methods: {
+			// 复制手机号码
+			copyPhoneNumber() {
+				if (this.phoneNumber) {
+					uni.setClipboardData({
+						data: this.phoneNumber
+					});
+				}
+			},
+			// 获取模板列表
+			getTemplateList() {
+				uni.request({
+					url: 'http://175.24.113.82:7001/api/getTemplateList',
+					header: {
+						'Authorization': this.userInfo.token
+					},
+					data: {
+						pageNo: 1,
+						pageSize: 100
+					},
+					method: 'POST',
+					dataType: 'json',
+					success: (res) => {
+						const { code, data, msg } = res.data
+						if(code === 0) {
+							this.templateList = data.data
+							if (this.templateList.length > 0) {
+								this.templateContent = this.templateList[0].content
+							}
+						} else {
+							if (code === 401) {
+								this.jumpLoginPage()
+							} else {
+								uni.showToast({
+									title: msg,
+									icon: 'none'
+								});
+							}
+						}
+					},
+				});
+			},
+			// 发送信息
+			sendMessage() {
+				if (uni.getSystemInfoSync().platform === 'android') {
+					const msg = plus.messaging.createMessage(plus.messaging.TYPE_SMS);
+					msg.to = ['18612186696'];
+					msg.body = this.salutation + this.templateContent;
+					plus.messaging.sendMessage( msg );
+				}
+			},
+			// 点击发送按钮
+			clickSendBtn() {
+				if (this.phoneInfo.phoneNumber) {
+					this.sendMessage()
+					this.updatePhone({
+						...this.phoneInfo,
+						sendPhoneNumber: this.username,
+						sendContent: this.salutation + this.templateContent,
+						sendTime: new Date(),
+						status: 'SENDED'
+					})
+				}
+				this.getNextPhoneNumber()
+			},
+			// 点击不发送按钮
+			clickNotSendBtn() {
+				if (this.phoneInfo.phoneNumber) {
+					debugger
+					this.updatePhone({
+						...this.phoneInfo,
+						status: 'INVALID'
+					})
+				}
+				this.getNextPhoneNumber()
+			},
+			// 获取下一个电话号码
+			getNextPhoneNumber() {
+				uni.request({
+					url: 'http://175.24.113.82:7001/api/getNextPhoneNumber',
+					header: {
+						'Authorization': this.userInfo.token
+					},
+					method: 'POST',
+					dataType: 'json',
+					success: (res) => {
+						const { code, data, msg } = res.data
+						if (res.data.code === 401) {
+							this.jumpLoginPage()
+							return
+						}
+						if(code === 0) {
+							const { _id, phoneNumber, area, sp } = data
+							this.phoneInfo = {
+								_id,
+								phoneNumber,
+								area,
+								sp
+							}
+						} else {
+							if (code === 401) {
+								this.jumpLoginPage()
+							} else {
+								uni.showToast({
+									title: msg,
+									icon: 'none'
+								});
+							}
+						}
+					}
+				});
+			},
+			// 更新手机号
+			updatePhone(data) {
+				uni.request({
+					url: 'http://175.24.113.82:7001/api/updatePhone',
+					header: {
+						'Authorization': this.userInfo.token
+					},
+					data: {
+						...data
+					},
+					method: 'POST',
+					dataType: 'json',
+					success: (res) => {
+						uni.hideLoading()
+						if (res.data.code === 401) {
+							this.jumpLoginPage()
+							return
+						}
+						if (res.data.code === 0) {
+							this.getNextPhoneNumber()
+						} else {
+							uni.showToast({
+								title: res.data.msg,
+								icon: 'none'
+							});
+						}
+					}
+				});
+			},
+			// 跳转登录页面
+			jumpLoginPage() {
+				uni.setStorageSync('userInfo', {
+					...this.userInfo,
+					token: ''
+				})
+				uni.redirectTo({
+					url: '/pages/login/login',
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	.container {
+		padding: 50rpx 30rpx;
+	}
+	.phone-wrap {
+		text-align: center;
+		color: #333;
+	}
+	.phone {
+		line-height: 50rpx;
+		font-weight: bold;
+		font-size: 50rpx;
+	}
+	.belong {
+		margin-top: 20rpx;
+		line-height: 40rpx;
+		font-size: 40rpx;
+	}
+	.salutation-wrap {
+		margin-top: 50rpx;
+		display: flex;
+		align-items: center;
+		font-size: 32rpx;
+	}
+	.salutation-label {
+		font-size: 32rpx;
+	}
+	.salutation-input {
+		flex: 1;
+		text-align: left;
+		font-size: 32rpx;
+		height: 50rpx;
+		color: #333333;
+		border-bottom: 0.5px solid #e2e2e2;
+	}
+	.btn-wrap {
+		margin-top: 30rpx;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
+	.btn {
+		margin: 10rpx;
+		flex: 1;
+	}
+	.btn:first-child {
+		margin-left: 0;
+	}
+	.btn:last-child {
+		margin-right: 0;
+	}
+	.template-content {
+		box-sizing: border-box;
+		margin-top: 20rpx;
+		display: block;
+		height: 400rpx;
+		width: 100%;
+		padding: 10rpx;
+		border-radius: 10rpx;
+		background-color: #F8F8F8;
+	}
+</style>
---
+++ pages/login/login.vue
@@ -0,0 +1,174 @@
+<template>
+	<view class="content">
+		<view class="header">
+			<image src="/static/logo.png"></image>
+		</view>
+		<view class="list">
+			<view class="list-call">
+				<image class="img" src="/static/phone.png"></image>
+				<input class="sl-input" v-model="userInfo.username" type="number" maxlength="11" placeholder="输入手机号" />
+			</view>
+			<view class="list-call">
+				<image class="img" src="/static/password.png"></image>
+				<input class="sl-input" v-model="userInfo.password" type="text" maxlength="32" placeholder="输入密码" password="true" />
+			</view>
+
+		</view>
+
+		<view class="button-login" hover-class="button-hover" @tap="bindLogin">
+			登录
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				userInfo: {
+					username: '',
+					password: ''
+				}
+			};
+		},
+		created() {
+			const {
+				token,
+				username,
+				password
+			} = uni.getStorageSync('userInfo') || {}
+			if (token) {
+				uni.redirectTo({
+					url: '/pages/index/index',
+				})
+				return
+			}
+			this.userInfo = {
+				username,
+				password
+			}
+		},
+		methods: {
+			bindLogin() {
+				uni.request({
+					url: 'http://175.24.113.82:7001/api/login',
+					data: this.userInfo,
+					method: 'POST',
+					dataType: 'json',
+					success: (res) => {
+						const {
+							code,
+							data,
+							msg
+						} = res.data
+						if (code === 0) {
+							uni.setStorageSync('userInfo', {
+								token: data.token,
+								username: data.userInfo.username || '',
+								password: data.userInfo.password || ''
+							})
+							uni.redirectTo({
+								url: '/pages/index/index',
+							})
+						} else {
+							uni.showToast({
+								title: msg,
+								icon: 'none'
+							});
+						}
+					}
+				});
+
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+	}
+
+	.header {
+		width: 161rpx;
+		height: 161rpx;
+		margin-top: 30rpx;
+		margin-left: auto;
+		margin-right: auto;
+	}
+
+	.header image {
+		width: 161rpx;
+		height: 161rpx;
+	}
+
+	.list {
+		display: flex;
+		flex-direction: column;
+		padding-top: 50rpx;
+		padding-left: 70rpx;
+		padding-right: 70rpx;
+	}
+
+	.list-call {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
+		height: 100rpx;
+		color: #333333;
+		border-bottom: 0.5px solid #e2e2e2;
+	}
+
+	.list-call .img {
+		width: 40rpx;
+		height: 40rpx;
+	}
+
+	.list-call .sl-input {
+		flex: 1;
+		text-align: left;
+		font-size: 32rpx;
+		margin-left: 16rpx;
+	}
+
+	.button-login {
+		color: #FFFFFF;
+		font-size: 34rpx;
+		width: 470rpx;
+		height: 100rpx;
+		background: linear-gradient(-90deg, rgba(63, 205, 235, 1), rgba(188, 226, 158, 1));
+		box-shadow: 0rpx 0rpx 13rpx 0rpx rgba(164, 217, 228, 0.2);
+		border-radius: 50rpx;
+		line-height: 100rpx;
+		text-align: center;
+		margin-left: auto;
+		margin-right: auto;
+		margin-top: 100rpx;
+	}
+
+	.button-hover {
+		background: linear-gradient(-90deg, rgba(63, 205, 235, 0.8), rgba(188, 226, 158, 0.8));
+	}
+
+	.agreenment {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+		font-size: 30rpx;
+		margin-top: 80rpx;
+		color: #FFA800;
+		text-align: center;
+		height: 40rpx;
+		line-height: 40rpx;
+	}
+
+	.agreenment text {
+		font-size: 24rpx;
+		margin-left: 15rpx;
+		margin-right: 15rpx;
+	}
+</style>
